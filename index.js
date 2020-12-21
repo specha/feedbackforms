f9f23b5cb712380a24856e09817fd765cfe3be22
Shikhar Mathur <chrome.shikhar@gmail.com>
2020-12-21 23:47:25 +0530

    Basic Structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const morgan=require('morgan');
+const helmet=require('helmet');
+
+const app=express();
+
+app.use(helmet());
+app.use(morgan('short'));
+app.use(express.json());
+app.use(express.static('./public'));
+app.listen(3000,(req,res)=>{
+    console.log(`Listening at http://localhost:3000`);
+})
